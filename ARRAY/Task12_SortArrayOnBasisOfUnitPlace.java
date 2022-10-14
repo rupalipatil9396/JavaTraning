bbeb1af72ecfdb46537b6df178adcc88e2391bf8
rupalipatil9396 <111984146+rupalipatil9396@users.noreply.github.com>
2022-10-14 15:17:06 +0530

    Add files via upload

---
+++ ARRAY/Task12_SortArrayOnBasisOfUnitPlace.java
@@ -0,0 +1,20 @@
+package com.yash.java.arrayprogrames;
+//WAP to sort array on the basis of unit place. For example we are having following
+//numbers:- 10,2,3,41,12,13,19,81,9. Output will be 10,41,81,2,12,3,13,19,9.
+public class Task12_SortArrayOnBasisOfUnitPlace {
+
+	    public static void main(String[] args) {
+	        int arr[]= {10,2,3,41,12,13,19,81,9};
+	        for(int i=0;i<10;i++)
+	        {
+	            for(int j=0;j<arr.length;j++)
+	            {
+	                if(arr[j]%10==i)
+	                    System.out.print(arr[j]+" ");
+	            }
+	            
+	        }
+	        
+	    }
+	}
+
