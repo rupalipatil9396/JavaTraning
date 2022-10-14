b1f46d15daf582475943242e4810758cec07da2e
rupalipatil9396 <111984146+rupalipatil9396@users.noreply.github.com>
2022-10-14 15:25:03 +0530

    Add files via upload

---
+++ JavaTraining/src/TestRun.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestRun { 
+	@AfterClass  
+    public static void tearDownAfterClass() throws Exception {  
+        System.out.println("After class");  
+    } 
+	/*class MyCode{
+    public static int printResult(){
+    		int a=12,b=23;
+    		System.out.println("Result = " +a+b);
+    		while(true);
+    	}
+    */
+	     
+}
